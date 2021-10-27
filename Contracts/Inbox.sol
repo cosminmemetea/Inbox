7a9e4a8fd47704237082a565014cc5adf9f73308
cosminmemetea <cosmin.memetea@gmail.com>
2021-10-27 06:47:42 +0300

    First successfully compiled contract

---
+++ Contracts/Inbox.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.4.16 <0.9.0;
+pragma abicoder v1;
+
+abstract contract Inbox {
+    function setMessage(string memory newMessage) public virtual;
+}
+
+contract InboxContract is Inbox{
+     string  public message;
+
+     constructor (string memory initialMessage) {
+         message  = initialMessage;
+     }
+
+     function setMessage ( string memory newMessage) public override{
+         message = newMessage;
+     }
+}
