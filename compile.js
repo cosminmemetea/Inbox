0978a64e9cb88b7bfc8eb03dd7d1e7ed0162d32e
cosminmemetea <cosmin.memetea@gmail.com>
2021-10-28 04:57:53 +0300

    Fixed compile.js file to return the correct intrface and bytecode

--- compile.js
+++ compile.js
@@ -20,6 +20,7 @@ var input = {
     }
 };
 const output = JSON.parse(solc.compile(JSON.stringify(input)));
-const interface = output.contracts['Inbox.sol'].interface;
-const bytecode = output.contracts['Inbox.sol'].bytecode;
-module.exports = {interface, bytecode}
+const interface = output.contracts['Inbox.sol']['InboxContract'].abi;
+const bytecode = output.contracts['Inbox.sol']['InboxContract'].evm.bytecode.object;
+
+module.exports = {interface, bytecode};
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,15 +2,17 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3  = require('web3');
 const web3 = new Web3(ganache.provider());
-const {inb} = require('../compile');
+const {interface, bytecode} = require('../compile');
 let accounts;
 let inbox;
 beforeEach(async () => {
     // Get a list of all accounts
     accounts = await web3.eth.getAccounts();
     // use one of the accounts to deploy the contract
-    inbox = await new web3.eth.Contract(JSON.parse(inb.interface))
-    .deploy({data: inb.bytecode, arguments:['Hi there']})
+    console.log('Compiled intrface is: --- ' + interface);
+    console.log('Compiled bytecode is: --- ' + bytecode);
+    inbox = await new web3.eth.Contract(interface)
+    .deploy({data: bytecode, arguments:['Hi there']})
     .send({from: accounts[0], gas: '1000000'});
 });
 
